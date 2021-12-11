808e27766f3274ccfe3dcce58a6eb764e221a9fe
Ömer Toraman <omer.t@mindbehind.com>
2021-12-11 18:44:54 +0300

    Add type def for the result of aggregation result

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -7,3 +7,23 @@ export type RecordSchema = {
       createdAt: Date
       value: string
 }
+
+/**
+ * Type alias describing the shape of the document as they come out of aggregation
+ */
+export type AggregationDoc = {
+      /**
+       * @example 'wtSjVcpg'
+       */
+      key: string
+
+      /**
+       * @example 2016-02-22T11:13:43.165Z
+       */
+      createdAt: Date
+
+      /**
+       * The sum of all elements in `counts` field
+       */
+      totalCount: number
+}

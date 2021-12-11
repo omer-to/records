029fc6e290ad972436d22f0d32417b87173ae7d7
Ömer Toraman <omer.t@mindbehind.com>
2021-12-11 17:38:50 +0300

    Add a stage builder for createdAt

--- src/lib/aggregation/records-stages/index.ts
+++ src/lib/aggregation/records-stages/index.ts
@@ -3,3 +3,22 @@ import * as stages from '../stages'
 /////////////////////////////////////////////////////////////////
 // Utility functions to create stages for the fields of interest
 /////////////////////////////////////////////////////////////////
+
+/**
+ * @description Creates a `$match` stage for `createdAt` field to match only the documents
+ * that fall into the specified date range.
+ * 
+ * It is intended to be used as the first stage in the pipeline,
+ * so as to be able to use the index on `createdAt` field.
+ *
+ * @param startDate The start date to be used in `$gt` operator
+ * @param endDate The end date to be used in `$lt` operator
+ */
+export function matchStageForCreatedDate(startDate: Date, endDate: Date) {
+      return stages.matchStageFrom({
+            createdAt: {
+                  $gt: startDate,
+                  $lt: endDate
+            }
+      })
+}
